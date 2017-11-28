808dfe450ce5da278cbb07cc2cdf4caf1844b64c
ssdwww <ssdwww@qq.com>
2017-11-28 23:29:16 +0800

    第二次修改

--- ud989-school/js/app.js
+++ ud989-school/js/app.js
@@ -86,13 +86,115 @@ $(function() {
 */
 
 
+
+
+$(function(){
+
+
+function checkmisday(name,alldays,defaultd){
+
+    var model = {
+        name : name,
+        alldays : alldays,
+        default : defaultd,
+        misday:0
+    }
+
+
+    var octopus = {
+        init:function(){
+            contorl.init()
+        },
+        getname:function(){
+            return model.name;
+        },
+        getalldays:function(){
+            return model.alldays;
+        },
+        getdefault:function(){
+            return model.default;
+        },
+        setmisday:function(){
+            return model.misday++;
+        },
+        getmisday:function(){
+          return model.misday;
+        }
+
+    }
+
+    var contorl = {
+        init:function(){
+            this.name = octopus.getname()
+            this.days = octopus.getalldays();
+            this.misday = octopus.getdefault();
+          //  this.modday = octopus.getmisday();
+            this.createTr();
+        },
+        createTr:function(){
+            var datelist = '',
+            misday = '',
+                hjk = '';
+            for(var i=0; i<this.days;i++){
+                datelist +=`<td class="attend-col"><input type="checkbox"></td>`
+            }
+            misday = `<td class="missed-col">${this.misday}</td>`
+            hjk = `<tr class="student"><td class="name-col">${name}</td>${datelist} ${misday}</tr>`;
+            var  tbody = document.getElementById("tbody");
+            tbody.innerHTML += hjk;
+            var tttt =$(hjk).find(".attend-col");
+
+            tttt.each(function(){
+               if(!$(this).prop('checked')){
+                    octopus.setmisday()
+                }
+            })
+            this.modday = octopus.getmisday();
+            var ghyji = $(".missed-col");
+            ghyji.text(this.modday)
+
+          //$(hjk).find(".missed-col").text(octopus.getmisday());
+
+        }
+    }
+    octopus.init();
+
+}
+
+var thj = new checkmisday('chenliang',12,0);
+var thj1 = new checkmisday('chenliang1',12,0);
+var thj2 = new checkmisday('chenliang2',12,0);
+var thj3 = new checkmisday('chenliang3',12,0);
+var thj4 = new checkmisday('chenliang4',12,0);
+
+
+
+
+
+
+
+//var ujk = new checkmisday('chenliang',12,0);
+
+
+//var thj2 = new this.createTr('chenliang',octopus.setalldays(),octopus.getmday());
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 var model = {
         misDayu:0,
         addday:0,
         day:12
-
 }
-
 var octopus = {
     init:function(){
         contorl.init()
@@ -102,52 +204,51 @@ var octopus = {
     },
     getmday:function(){
         return model.misDayu
+    },
+    setalldays:function(){
+        return model.day
     }
 }
+
 
 var contorl = {
     init:function(){
         this.render();
-
     },
     render:function(){
-        var thj = new this.createTr('chenliang',12,0);
-        var thj = new this.createTr('zhangyang',12,0);
-        var thj = new this.createTr('dongshiqing',12,0);
-        var thj = new this.createTr('liuhuan',12,0);
+        var thj = new this.createTr('chenliang',octopus.setalldays(),octopus.getmday());
+        var thj2 = new this.createTr('chenliang',octopus.setalldays(),octopus.getmday());
     },
     createTr:function(name,days,misday){
        this.name = name;
        this.days = days;
        this.misday = misday;
-
        var datelist = '';
        var misday = '';
         var hjk = '';
-
         for(var i=0; i<this.days;i++){
             datelist +=`<td class="attend-col"><input type="checkbox"></td>`
         }
-
-
-
-
         misday = `<td class="missed-col">${this.misday}</td>`
-
-       hjk = `<tr class="student"><td class="name-col">${name}</td>${datelist} ${misday}</tr>`;
+        hjk = `<tr class="student"><td class="name-col">${name}</td>${datelist} ${misday}</tr>`;
         var  tbody = document.getElementById("tbody");
         tbody.innerHTML += hjk;
+        var tttt = $(".attend-col");
 
+        tttt.each(function(){
+            if(!$(this).prop('checked')){
+                octopus.setmday();
+            }
+        })
+
+        console.log(octopus.getmday())
 
 
     }
+
+
 }
 
+octopus.init();*/
 
-
-
-
-
-
-
-octopus.init();
+})
